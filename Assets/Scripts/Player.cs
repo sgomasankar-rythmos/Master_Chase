2598a53317b77d44d2e677b4f1ee2f502d41d882
sgomasankar <sgomasankar@rythmos.com>
2023-07-14 13:00:58 +0530

    Making the Camera to follow up the player.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -85,7 +85,7 @@ public class Player : MonoBehaviour
 
     void PlayerJump()
     {
-        if (Input.GetButtonDown("Jump")&& isGrounded)
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
             isGrounded = false;
             myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
